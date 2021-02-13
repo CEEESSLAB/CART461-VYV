1778e2a6037c01b1f008e22256ef05682ca02ae4
a-nomalme <62852884+a-nomalme@users.noreply.github.com>
2021-02-13 15:11:18 -0500

    Brute force update of #include <> declarations

---
+++ SOUND-SYNTHESIS/MOZZI/lib/Mozzi/src/samples/empty_0_int8.h
@@ -0,0 +1,21 @@
+#ifndef EMPTY_0_H_
+#define EMPTY_0_H_
+ 
+#if defined(PARTICLE)
+  #include <Particle.h>
+  #include <Arduino.h>
+#elif defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+  #include "Print.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include "mozzi_pgmspace.h"
+ 
+#define EMPTY_0_NUM_CELLS 0
+#define EMPTY_0_SAMPLERATE 16384
+ 
+CONSTTABLE_STORAGE(int8_t) EMPTY_0_DATA [] = { };
+ 
+ #endif /* EMPTY_0_H_ */
